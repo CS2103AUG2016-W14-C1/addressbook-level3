9d6e25cc08dbaedc5d01362215e3d4770935e20d
Lam Chi Thanh <lamchithanh1997@gmail.com>
2016-09-25 21:25:33 +0800

    Add sort parameter to ListCommand constructor

--- src/seedu/addressbook/commands/ListCommand.java
+++ src/seedu/addressbook/commands/ListCommand.java
@@ -15,11 +15,24 @@ public class ListCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
             + "Displays all persons in the address book as a list with index numbers.\n\t"
             + "Example: " + COMMAND_WORD;
+    
+    public static final String SORT_OPTION_WORD = "-sort";
+    
+    public static final String SORT_ASC_WORD = "asc";
+    
+    public static final String SORT_DESC_WORD = "desc";
+    
+    public enum SortType { ASC, DESC, NONE };
+    
+    private SortType sortType;
 
-
+    public ListCommand (SortType sortType) {
+    	this.sortType = sortType;
+    }
+    
     @Override
     public CommandResult execute() {
-        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons(sortType).immutableListView();
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
     }
 }
